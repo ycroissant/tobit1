4e17ce9ba7d77b5d0183123b41e36fd09fee9ca1
ycroissant <yves.croissant@univ-reunion.fr>
2021-09-08 11:08:46 +0400

    smbltest is added a specific file for tests is created

---
+++ R/tests.R
@@ -0,0 +1,88 @@
+#' Hausman test
+#'
+#' Hausman test 
+#'
+#' @name haustest
+#' @param x the first model,
+#' @param y the second model
+#' @param omit a character containing the effects that are removed from the test
+#' @author Yves Croissant
+#' @importFrom stats pchisq
+#' \insertRet{HAUS:78}
+#' @examples
+#' library("tidyverse")
+#' charitable <- charitable %>% mutate(logdon = log(donation) - log(25))
+#' char_form <- logdon ~ log(donparents) + log(income) +
+#'     education + religion + married + south
+#' ml <- tobit1(char_form, data = charitable)
+#' scls <- update(ml, method = "trimmed")
+#' haustest(scls, ml, omit = "(Intercept)")
+#' @export
+haustest <- function(x, y, omit = NULL){
+    .data.name <- paste(paste(deparse(substitute(x))), "vs",
+                       paste(deparse(substitute(y))))
+    nms_x <- names(coef(x))
+    nms_y <- names(coef(y))
+    nms <- intersect(nms_x, nms_y)
+    unkn <- setdiff(omit, nms)
+    if (length(unkn)){
+        effects_string <- ifelse(length(unkn) == 1, "effect", "effects")
+        stop(cat(paste(paste(unkn, collapse = ", "), effects_string, "unknown\n"), sep = ""))
+    }
+    nms <- setdiff(nms, omit)
+    delta <- coef(x)[nms] - coef(y)[nms]
+    V <- vcov(x)[nms, nms] - vcov(y)[nms, nms]
+    stat <- as.numeric(crossprod(solve(V, delta), delta))
+    pval <- pchisq(stat, df = length(delta), lower.tail = FALSE)
+    res <- list(statistic    = c(chisq = stat),
+                p.value      = pval,
+                parameter    = c(df = length(delta)),
+                method       = "Hausman Test",
+                data.name    = .data.name,
+ #             null.value  = null.value,
+                alternative  = "the second model is inconsistent")
+    class(res) <- "htest"
+    return(res)
+}
+
+#' Smith and Blundell Test
+#'
+#' Test for Exogeneity in Tobit models
+#'
+#' @name smbltest
+#' @param formula a two part formula, with the instruments in the
+#'     second part
+#' @param data a data.frame
+#' @author Yves Croissant
+#' @importFrom stats pchisq
+#' @references
+#' \insertRet{SMIT:BLUN:86}
+#' @examples
+#' inst <- ~ sic3 + k_serv + inv + engsci + whitecol + skill + semskill + cropland + 
+#'     pasture + forest + coal + petro + minerals + scrconc + bcrconc +
+#'     scrcomp + bcrcomp + meps + 
+#'     kstock + puni + geog2 + tenure + klratio + bunion
+#' tradeprotection <- tradeprotection %>%
+#'    mutate(y = ntb / (1 + ntb),
+#'           x1 = exports / imports / elast,
+#'           x2 = cap * x1)
+#' smbltest(as.Formula(y ~ x1 + x2 + labvar, inst), tradeprotection)
+#' @export
+smbltest <- function(formula, data){
+    .data.name <- paste(deparse(substitute(formula)))
+    x <- ivtobit(formula, data, method = "2steps", robust = FALSE)
+    coef_res <- grep("resid_", names(coef(x)))
+    B <- coef(x)[coef_res]
+    V <- vcov(x)[coef_res, coef_res, drop = FALSE]
+    stat <- as.numeric(crossprod(B,solve(V, B)))
+    pval <- pchisq(stat, df = length(coef_res), lower.tail = FALSE)
+    res <- list(statistic    = c(chisq = stat),
+                p.value      = pval,
+                parameter    = c(df = length(coef_res)),
+                method       = "Smith-Blundell test",
+                data.name    = .data.name,
+ #             null.value  = null.value,
+                alternative  = "endogeneity")
+    class(res) <- "htest"
+    return(res)
+}
---
+++ tests/testthat/test-gradient_hessian.R
@@ -0,0 +1,50 @@
+context("analytical vs numerical gradient / hessian")
+
+
+check <- function(f, param, y, X, sample, left, right, print = FALSE){
+    lnl <- f(param, y = y, X = X, sample = sample, left = left, right = right, gradient = TRUE, hessian = TRUE)
+    an_grad <- attr(lnl, "gradient")
+    an_hess <- attr(lnl, "hessian")
+    num_grad <- numDeriv::grad(f, param, y = y, X = X, sample = sample, left = left, right = right)
+    num_hess <- numDeriv::hessian(f, param, y = y, X = X, sample = sample, left = left, right = right)
+    if (print){
+        cat(paste("sample:", sample, "\n"))
+        cat(paste("truncation points:",left, "-", right, "\n"))
+        cat(paste("log-likelihood:", as.numeric(lnl), "\n"))
+        cat("gradient\n")
+        print(cbind(num = num_grad, an = an_grad))
+        cat("hessian\n")
+        cat("numerical:\n")
+        print(num_hess)
+        cat("analytical:\n")
+        print(an_hess)
+        cat("\n\n")
+    }
+    c(all(dplyr::near(an_grad, num_grad, tol = 1E-05)), all(dplyr::near(an_hess, num_hess, tol = 1E-05)))
+}
+
+check_grad_hess <- function(f, param, y, X, sample, left, right, print = FALSE){
+    lnl <- f(param, y = y, X = X, sample = sample, left = left, right = right, gradient = TRUE, hessian = TRUE)
+    an_grad <- attr(lnl, "gradient")
+    an_hess <- attr(lnl, "hessian")
+    num_grad <- numDeriv::grad(f, param, y = y, X = X, sample = sample, left = left, right = right)
+    num_hess <- numDeriv::hessian(f, param, y = y, X = X, sample = sample, left = left, right = right)
+    all(dplyr::near(an_grad, num_grad, tol = 1E-05), dplyr::near(an_hess, num_hess, tol = 1E-05))
+}
+
+    
+test_that("grad_hess", {
+    data("feesadm", package = "tobit1")
+    mf <- model.frame(fees ~ expense, feesadm)
+    X <- model.matrix(fees ~ expense, mf)
+    y <- model.response(mf)
+    set.seed(1)
+    coefs <- runif(3)
+    coefs_olsen <- c(coefs[1:2] / coefs[3], 1 / coefs[3])
+    testthat::expect_true(check_grad_hess(lnl_tp, coefs, y, X, "censored", left = 0, right = Inf))
+    testthat::expect_true(check_grad_hess(lnl_tp, coefs, y, X, "censored", left = 0, right = 10))
+    testthat::expect_true(check_grad_hess(lnl_tp, coefs, y, X, "censored", left = -Inf, right = 10))
+    testthat::expect_true(check_grad_hess(lnl_tp, coefs, y, X, "truncated", left = 0, right = Inf))
+    testthat::expect_true(check_grad_hess(lnl_tp, coefs, y, X, "truncated", left = -Inf, right = 10))
+}
+)
